dc2d1ff334a27b1bab9d367881b50b69e035291e
Angelo Chagas <angelo.chagas@live.com>
2021-08-25 16:55:22 -0300

    first commit (boilerplate with theme)

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import theme from '../../theme';
+
+export const Container = styled.div`
+  background: ${theme.colors.background};
+  color: ${theme.colors.primary};
+`;
