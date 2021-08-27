3290133452ff29819d2efa3351569e8baca0e3d4
Angelo <angelo.chagas@live.com>
2021-08-27 10:18:18 -0300

    adds logo and search input field

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Container, Search } from './styles';
 
 export function Home() {
-  return <Container>Hello World</Container>;
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Container>
+      <Search>
+        <img src={logo} alt="Logotipo" />
+        <TextField label="Search" outlined>
+          <Input
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+        </TextField>
+      </Search>
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  // background: ${(props) => props.theme.colors.primary};
+export const Container = styled.aside`
+  background: ${(props) => props.theme.colors.primary};
+  width: 360px;
+  height: 100vh;
+  overflow-y: hidden;
+`;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #fff;
+  padding: 16px;
 `;
